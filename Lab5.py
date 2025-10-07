b390581581c708a17b298ad68e193ea10689a606
Nishan-567 <nsandhu@terpmail.umd.edu>
2025-10-07 15:32:53 -0400

    Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -10,7 +10,7 @@ pins = [] #pin numbers
 f = 500 #frequency in Hz
 #initialize all pins
 for i in range(10):
-	GPIO.setup(pin[i], GPIO.OUT)
+	GPIO.setup(pins[i], GPIO.OUT)
 
 
 dc_f = .2 # duty cycle B
@@ -18,7 +18,7 @@ t = time.time()
 dc1 = (math.sin(2*math.pi*dc_f*t))**2 #pwm duty cycle
 
 
-pwm = GPIO.PWM(pin[0], f)
+pwm = GPIO.PWM(pins[0], f)
 
 pwm.start(dc1)
 
@@ -26,7 +26,8 @@ pwm.start(dc1)
 phi = math.pi/11
 dc2 = (math.sin(2*math.pi*dc_f*t - phi))**2
 
-pwm = GPIO.PWM(pin[1], f)
+pwm = GPIO.PWM(pins[1], f)
 
 
 pwm.start(dc2)
+
